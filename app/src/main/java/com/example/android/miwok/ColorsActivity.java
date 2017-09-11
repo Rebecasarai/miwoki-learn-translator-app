d8a256fc60c33fb361f2fea5806e392687c3b954
rebecasarai <callmebeca@gmail.com>
2017-09-11 22:31:57 +0200

    more

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,14 +15,40 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.activity_numbers);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("One", "Lutti"));
+        words.add(new Word("Two", "Oṭiiko"));
+        words.add(new Word("Three", "Tolookosu"));
+        words.add(new Word("Four", "Oyyiisa"));
+        words.add(new Word("Five", "massokka"));
+        words.add(new Word("Six", "temmokka"));
+        words.add(new Word("Seven", "kenekaku"));
+        words.add(new Word("Eight", "kawinṭa"));
+        words.add(new Word("Nine", "wo'e"));
+        words.add(new Word("Ten", "na'aacha"));
+
+
+
+        int index=0;
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
